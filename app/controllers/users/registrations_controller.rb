7e7b6b5e446dd68cb0a7200243f63ded81a82e50
bala jee <balag3045@gmail.com>
2018-01-27 10:54:43 +0530

    Added notice

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-	def create
-		super do |resource|
-			if params[:plan]
-				resource.plan_id=params[:plan]
-				if resource.plan_id==2
-					resource.save_with_payment
-				else
-					reasource.save
-				end	
+	before_action :select_plan,only: :new
+		def create
+			super do |resource|
+				if params[:plan]
+					resource.plan_id=params[:plan]
+					if resource.plan_id==2
+						resource.save_with_payment
+					else
+						reasource.save
+					end	
+				end
 			end
 		end
-	end
+		private
+			def select_plan
+				unless params[:plan] && (params[:plan]=='1' || params[:plan]=='2')
+				flash[:notice]="Please select a membership plan"
+				redirect_to root_url
+				end
+			end
 end
